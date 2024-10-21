9ebe016f4e55f9d45c383a8752615941cbeeb52b
AlenaVP Legion <poprugo@gmail.com>
2024-10-21 17:22:10 +0200

    feat: implement GET /api/users/{userId}

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,6 +1,27 @@
+import{ validate as isUuid } from 'uuid';
+
 const users = [];
 
 export const getUsers = (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(users));
+};
+
+export const getUserById = (req, res, userId) => {
+  if (!isUuid(userId)) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Invalid user ID' }));
+    return;
+  }
+
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User not found' }));
+    return;
+  }
+
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(users));
 };
--- src/utils/router.js
+++ src/utils/router.js
@@ -7,6 +7,9 @@ const router = (req, res) => {
 
   if (pathName === '/api/users' && req.method === 'GET') {
     getUsers(req,res);
+  } else if (pathName.match(/^\/api\/users\/\w+$/) && req.method === 'GET') {
+    const userId = pathName.split('/')[3];
+    getUserById(req, res, userId);
   } else {
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ message: 'Not Found' }));
